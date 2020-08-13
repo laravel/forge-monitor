d58713d59f1de10cfb79c6a42a36443603802650
James Brooks <james@alt-three.com>
2020-08-13 13:31:56 +0100

    wip

--- app/Stats/AbstractStat.php
+++ app/Stats/AbstractStat.php
@@ -50,7 +50,7 @@ abstract class AbstractStat
         $this->totalResults = count($results);
 
         // Not enough data to check.
-        if ($this->totalResults < $this->monitor->minutes) {
+        if ($this->monitor->minutes > 0 && $this->totalResults < $this->monitor->minutes) {
             return false;
         }
 
