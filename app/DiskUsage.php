764448b01042277f3e6e59f2fe7b1a4ad0a9a2db
James Brooks <james@alt-three.com>
2020-08-13 12:25:19 +0100

    Remove unused date casts

--- app/DiskUsage.php
+++ app/DiskUsage.php
@@ -7,13 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class DiskUsage extends Model
 {
     /**
-     * The attributes that should be mutated to dates.
-     *
-     * @var array
-     */
-    protected $dates = ['sampled_at'];
-
-    /**
      * The attributes that aren't mass assignable.
      *
      * @var array
