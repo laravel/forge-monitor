a7086d2e29a2fc99622dfc7ec3ebd49900d189f6
James Brooks <james@alt-three.com>
2022-05-09 11:46:07 +0100

    anonymous migrations

--- database/migrations/2020_03_25_102333_create_alerts_table.php
+++ database/migrations/2020_03_25_102333_create_alerts_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateAlertsTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
@@ -23,4 +23,4 @@ class CreateAlertsTable extends Migration
             $table->index('created_at');
         });
     }
-}
+};
